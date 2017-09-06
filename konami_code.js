e127478a3d6e4fd865f844b3bb54307fce5f2307
Nathan Campbell <nathancampbell@outlook.com>
2017-09-06 19:02:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,19 +2,23 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 let index = 0;
 
-function init(e) {
-  const key = parseInt(e.detail || e.which);
+function init() {
+  const input = document.querySelector('body')
 
-  if (key === code[index]) {
-    index++;
+  input.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
 
-    if (index === code.length) {
-      alert("Hurray!");
+    if (key === code[index]) {
+      index++;
 
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
-  }
+  })
 
 }
